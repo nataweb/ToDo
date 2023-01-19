aef3bc35ede01c0e0ce8330745bc1a1b662ff4b8
Natalia <besarabnatalia777@gmail.com>
2023-01-19 19:08:58 +0300

    Add project files.

---
+++ ToDo.API/Controllers/ToDoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo.API.Models;
+
+namespace ToDo.API.Controllers
+{
+    [ApiController]
+    public class ToDoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public ToDoController(TodoContext context)
+        {
+           _context = context;
+        }
+        //public IActionResult GetTodoItem()
+        //{
+        //    var context = _context.GetTodoItem();
+        //    if (context == null)
+        //    {
+        //        return NotFound();
+        //    }
+            
+        //    return Ok(context);
+        //}
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+        
+    }
+}
